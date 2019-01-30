12d35e89adcf568ddeabbe182efdc37e13a187d5
Diego Matus <dmatus15@alumnos.utalca.cl>
2019-01-30 20:34:54 -0300

    Se implemento la mantención de session
    -Se agrego check box para recordar sesion.
    -Se guardo estado de checkbox a un SharedPreferences.
    -Se agrego metodo obtenerEstadoRecordarSesion y guardarEstadoRecordarSesion.

--- app/src/main/java/com/e/appmc/MainActivity.java
+++ app/src/main/java/com/e/appmc/MainActivity.java
@@ -1,17 +1,26 @@
 package com.e.appmc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText usuario;
     private EditText contraseña;
+    private CheckBox session;
+    private boolean estaActivadoCheckBox;
+    private static final String SESSION_ESTADO_RECORDAR = "estado_recordado";
+    private static final String ESTADO_CHECK_BOX = "estado_check";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,36 @@ public class MainActivity extends AppCompatActivity {
         /*seteo de instancias de los objetos*/
         usuario = (EditText)findViewById(R.id.EditUsuario);
         contraseña = (EditText)findViewById(R.id.EditContraseña);
+        session = (CheckBox) findViewById(R.id.checkbox_session);
+
+        estaActivadoCheckBox = session.isChecked();
+
+        if (obtenerEstadoRecordarSession()) enterSession();
+
+    }
+
+
+    public void activarGuardarSesion(View view)
+    {
+        if (estaActivadoCheckBox)
+        {
+            session.setChecked(false);
+        }
+
+        estaActivadoCheckBox = session.isChecked();
+    }
+
+
+    public  void guadarEstadoRecordarSesion()
+    {
+        SharedPreferences sesionPreferencias = getSharedPreferences(SESSION_ESTADO_RECORDAR,MainActivity.MODE_PRIVATE);
+        sesionPreferencias.edit().putBoolean(ESTADO_CHECK_BOX,session.isChecked()).apply();
+    }
+
+    public boolean obtenerEstadoRecordarSession()
+    {
+        SharedPreferences sesionPreferencias = getSharedPreferences(SESSION_ESTADO_RECORDAR, MainActivity.MODE_PRIVATE);
+        return sesionPreferencias.getBoolean(ESTADO_CHECK_BOX,false);
     }
 
     public void logear(View view)
@@ -39,9 +78,7 @@ public class MainActivity extends AppCompatActivity {
             int opcion = this.validar(usr,pass);
             switch(opcion){
                 case 0:
-                    Intent intent = new Intent(this,MainMenuActivity.class);
-                    //intent.putExtra(EXTRA_MESSAGE, usr);
-                    startActivity(intent);
+                        enterSession();
                     break;
                 case 1:
                     usuario.setError("Usuario Incorrecto");
@@ -67,5 +104,16 @@ public class MainActivity extends AppCompatActivity {
             return 2;
         }
         return 0;
+    }
+
+
+
+
+    private  void enterSession() {
+            guadarEstadoRecordarSesion();
+            Intent intent = new Intent(MainActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+            finish();
+
     }
 }
