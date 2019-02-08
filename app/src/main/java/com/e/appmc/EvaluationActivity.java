944a975b954d051ff157e37f7ded4e1ee9424d99
Ariel <arcornejo15@alumnos.utalca.cl>
2019-02-07 23:08:15 -0300

    Funcionalidad Boton Centro y Boton lista Personal, Actividad Evaluacion
    Se agrego funcionalidad en el botón centro para lograr seleccionar estos y ademas en el botón lista de personal asociado al centro en el cual se este trabajando.

--- app/src/main/java/com/e/appmc/EvaluationActivity.java
+++ app/src/main/java/com/e/appmc/EvaluationActivity.java
@@ -1,6 +1,7 @@
 
 package com.e.appmc;
 
+import android.app.AlertDialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
@@ -10,26 +11,65 @@ import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.e.appmc.dummy.DummyContent;
+import com.e.bd.appmc.Facility;
+import com.e.bd.appmc.Personal;
 import com.e.bd.appmc.SQLiteOpenHelperDataBase;
+
+import java.util.ArrayList;
 
 public class EvaluationActivity extends AppCompatActivity implements FragmentFiveDimension.OnFragmentInteractionListener , SecurityDimensionFragment.OnFragmentInteractionListener {
 
     private Button buttonElegirUbicacionCentro;
-    //private FloatingActionButton buttonListaUsuarios;
     private FragmentFiveDimension fragmentoCincoDimensiones;
     private SecurityDimensionFragment fragmentoCuatroDimensiones;
+    private Spinner centroActual;
+    private SQLiteOpenHelperDataBase bd;
 
     private int idUsuario;
+    private int idCentroActual;
+    private FailitySpinnerAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluation);
-        //buttonListaUsuarios = (Button)findViewById(R.id.fab);
         idUsuario = getIntent().getExtras().getInt("id");
+        bd =  new SQLiteOpenHelperDataBase(this,"mcapp",null,1);
+        centroActual = (Spinner)findViewById(R.id.centroActual);
+        adapter = new FailitySpinnerAdapter(this,R.layout.spinner_facility_item,this.obtnereCentros());
+        centroActual.setAdapter(adapter);
+        centroActual.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, final int i, long l) {
+                // Here you get the current item (a User object) that is selected by its position
+                Facility user = adapter.getItem(i);
+                // Here you can do the action you want to...
+                Toast.makeText(EvaluationActivity.this, "ID: " + user.getId() + "\nName: " + user.getName(),
+                        Toast.LENGTH_SHORT).show();
+                idCentroActual = user.getId();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         int opcion = comprobarServicio();
         if(opcion ==1)
         {
@@ -43,6 +83,7 @@ public class EvaluationActivity extends AppCompatActivity implements FragmentFiv
         }
 
 
+
     }
 
     @Override
@@ -52,9 +93,8 @@ public class EvaluationActivity extends AppCompatActivity implements FragmentFiv
 
     private int comprobarServicio()
     {
-        SQLiteOpenHelperDataBase db = new SQLiteOpenHelperDataBase(this,"mcapp",null,1);
         String query = "SELECT service_id FROM facility WHERE user_Id = " + idUsuario;
-        Cursor servicio =db.doSelectQuery(query);
+        Cursor servicio =bd.doSelectQuery(query);
         if(servicio.getCount()>0)
         {
             servicio.moveToFirst();
@@ -67,16 +107,75 @@ public class EvaluationActivity extends AppCompatActivity implements FragmentFiv
 
     public void listaPersonal(View view)
     {
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View customView = layoutInflater.inflate(R.layout.personal_container,null);
 
-        DialogFragment dialogFragment = new PersonalListFragment();
-        dialogFragment.show(ft, "dialog");
+        ArrayList<Personal> personals = rellenarPersonal();
+        PersonalAdapter personal = new PersonalAdapter(personals);
+
+        RecyclerView list = (RecyclerView) customView;
+        list.setAdapter(personal);
+        list.setItemAnimator(new DefaultItemAnimator());
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+
+        builder.setView(customView);
+        builder.create();
+        builder.show();
     }
+
+    public Facility[] obtnereCentros()
+    {
+        //Adaptar para usar con GPS
+
+        String query = "SELECT * FROM facility WHERE user_id = " + idUsuario;
+        Cursor data = bd.doSelectQuery(query);
+        Facility[] centros = new Facility[data.getCount()];
+        if(data.moveToFirst())
+        {
+            int i =0;
+            do{
+                int id = data.getInt(data.getColumnIndex("id"));
+                //int user_id= data.getInt(data.getColumnIndex("user_id"));
+                String created = data.getString(data.getColumnIndex("created"));
+                String code = data.getString(data.getColumnIndex("code"));
+                String name = data.getString(data.getColumnIndex("name"));
+                String address = data.getString(data.getColumnIndex("address"));
+                int service_id = data.getInt(data.getColumnIndex("service_id"));
+                centros[i] = new Facility(id,idUsuario,created,code,name,address,service_id);
+                i=i+1;
+            }while(data.moveToNext());
+
+        }
+
+        return centros;
+
+    }
+
+    public ArrayList<Personal> rellenarPersonal()
+    {
+        ArrayList<Personal> personal = new ArrayList<>();
+        String query = "SELECT * FROM personal WHERE facility_id = " + idCentroActual;
+        Cursor data=bd.doSelectQuery(query);
+        if(data.moveToFirst())
+        {
+            do{
+                int id= data.getInt(data.getColumnIndex("id"));
+                String name =data.getString(data.getColumnIndex("name"));
+                String surname = data.getString(data.getColumnIndex("surname"));
+                String rut = data.getString(data.getColumnIndex("rut"));
+                personal.add(new Personal(id,name,surname,rut,"","",idCentroActual));
+
+            }while(data.moveToNext());
+
+        }
+
+        return personal;
+
+    }
+
+
 
 
 
--- app/src/main/java/com/e/appmc/PersonalAdapter.java
+++ app/src/main/java/com/e/appmc/PersonalAdapter.java
@@ -7,10 +7,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.e.bd.appmc.Personal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonalAdapter extends RecyclerView.Adapter<PersonalAdapter.ViewHolder>
@@ -19,23 +23,36 @@ public class PersonalAdapter extends RecyclerView.Adapter<PersonalAdapter.ViewHo
     Context context;
 
     public PersonalAdapter(List<Personal> personalList) {
-        personalList = personalList;
+        this.personalList = personalList;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,viewGroup,false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_personal,viewGroup,false);
         ViewHolder viewHolder = new ViewHolder(view);
         context = viewGroup.getContext();
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
         Personal personal = personalList.get(i);
 
         viewHolder.textRut.setText(personal.getRut());
-        viewHolder.textName.setText(personal.getName());
+        viewHolder.textName.setText(personal.getName() + " " + personal.getSurname());
+        viewHolder.cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(context,"The position is:"+i,Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"The position is:"+i,Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -49,6 +66,7 @@ public class PersonalAdapter extends RecyclerView.Adapter<PersonalAdapter.ViewHo
         TextView textRut;
         TextView textName;
         CardView cv;
+        ImageButton  delete;
 
         public ViewHolder(View itemView)
         {
@@ -56,6 +74,7 @@ public class PersonalAdapter extends RecyclerView.Adapter<PersonalAdapter.ViewHo
             textRut = (TextView) itemView.findViewById(R.id.textRut);
             textName = (TextView)itemView.findViewById(R.id.textName);
             cv = (CardView)itemView.findViewById(R.id.cv);
+            delete = (ImageButton)itemView.findViewById(R.id.delete);
         }
     }
 }
